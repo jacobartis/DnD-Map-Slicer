3c04cf0b63ec4d1e8fb4eabaa6e1a6c1371d8385
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-07-28 21:57:06 +0100

    Created simple functions to draw a virticle or horizontal line in the image.

--- img_test.py
+++ img_test.py
@@ -1,5 +1,12 @@
 import cv2 as cv
-import sys
+import numpy as np
+
+#Draws a vertical
+def vertical_line(img:cv.Mat, colour:np.array, pos:int, size:int):
+    img[0:img.shape[0],pos:pos+size] = colour
+
+def horizontal_line(img:cv.Mat, colour:np.array, pos:int, size:int):
+    img[pos:pos+size,0:img.shape[1]] = colour
 
 #Generates img obj reading from file
 img = cv.imread(cv.samples.findFile("test_img.png"))
@@ -7,6 +14,10 @@ img = cv.imread(cv.samples.findFile("test_img.png"))
 #Exits if img is invalid
 if img is None:
     quit()
+size = img.shape[:2]
+print(size)
+vertical_line(img,np.array([255,0,0]),int(size[0]/2),5)
+horizontal_line(img,np.array([255,0,0]),int(size[1]/2),5)
 
 #Displays the img
 cv.imshow("Display window",img)
